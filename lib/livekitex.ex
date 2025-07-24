2829511b33e45a7c1ba129b92fd053070bd2a43e
Dinko Azema <rocket4ce@gmail.com>
2025-07-24 17:09:27 -0400

    first commit

---
+++ lib/livekitex.ex
@@ -0,0 +1,18 @@
+defmodule Livekitex do
+  @moduledoc """
+  Documentation for `Livekitex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Livekitex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/livekitex_test.exs
@@ -0,0 +1,8 @@
+defmodule LivekitexTest do
+  use ExUnit.Case
+  doctest Livekitex
+
+  test "greets the world" do
+    assert Livekitex.hello() == :world
+  end
+end
