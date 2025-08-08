9e4188f821727a4d06c741f3664f9e031f26bd07
Dinko Azema <rocket4ce@gmail.com>
2025-08-08 03:44:48 -0400

    fix: Correct indentation for version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Livekitex.MixProject do
   def project do
     [
       app: :livekitex,
-  version: "0.1.3",
+      version: "0.1.3",
       elixir: "~> 1.18",
       deps: deps(),
       description: description(),
